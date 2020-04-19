6601c7790acb4081b7a332f4d6b7c26b77fd0441
Jaum Vitor Pires PC <joao.vitor.pires.060@gmail.com>
2020-04-19 03:20:17 -0300

    Ultima modificação do teste

--- ProjetoTeste/src/pacote/Exemplo.java
+++ ProjetoTeste/src/pacote/Exemplo.java
@@ -5,5 +5,7 @@ public class Exemplo {
 	public static void main(String[] args) {
 		System.out.println("Criando meu projeto de teste");
 		System.out.println("Fazendo modificações no codigo");
+		
+		System.out.println("Mod");
 	}
 }
