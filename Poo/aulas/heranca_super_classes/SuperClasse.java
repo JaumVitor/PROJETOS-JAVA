da63c2ae08894372dc5c4758729d2d773e3f4bda
Jaum Vitor Pires PC <joao.vitor.pires.060@gmail.com>
2020-05-21 00:20:32 -0300

    Testes da super Classe e SubClasse java

---
+++ Poo/aulas/heranca_super_classes/SuperClasse.java
@@ -0,0 +1,33 @@
+package heranca_super_classes;
+
+public class SuperClasse {
+	public int a; 
+	protected int b;
+	
+	public void print() {
+		System.out.println("Usando print da super Classe");
+		System.out.println(this.a);
+		System.out.println(this.b);
+	}
+	
+	public SuperClasse(int a, int b) { //Contrutor da superClasse
+		this.a = a;
+		this.b = b;
+	}
+
+	public int getA() {
+		return a;
+	}
+
+	public void setA(int a) {
+		this.a = a;
+	}
+
+	public int getB() {
+		return b;
+	}
+
+	public void setB(int b) {
+		this.b = b;
+	} 
+}
---
+++ Poo/aulas/heranca_super_classes/TestesSuperClasse.java
@@ -0,0 +1,11 @@
+package heranca_super_classes;
+
+public class TestesSuperClasse {
+	public static void main(String[] args) {
+		SuperClasse superC = new SuperClasse(10,20); //Caso a superClasse seja abstract, não podemos estancia-la
+		SubClasse subC = new SubClasse(1, 2, 0, 0);  
+		
+		subC.print();
+		superC.print();
+	}
+}
