0a4266155f192800c851762a00494f006842dfd5
Jaum Vitor Pires PC <joao.vitor.pires.060@gmail.com>
2020-06-02 02:26:51 -0300

    Finalizando outra versao estavel na branch teste

--- Poo/aulas/banco_loiane/GerenciaConta.java
+++ Poo/aulas/banco_loiane/GerenciaConta.java
@@ -2,9 +2,20 @@ package banco_loiane;
 
 public class GerenciaConta {
 	private Conta[] vetor = new Conta[3];
+	private int cont=0;
 	
 	public void criarConta (int num) {
 		Conta x = new Conta (num);
-		vetor[0] = x ;
+		this.vetor[cont] = x ;
+		this.cont++; 
+	}
+	
+	public Conta buscar (int numeroConta) {
+		for (int i=0; i<this.cont; i++) {
+			if (this.vetor[i].getNumero() == numeroConta) {
+				return vetor[i];
+			}
+		}
+		return null; 
 	}
 }
