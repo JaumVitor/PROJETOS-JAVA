b59be622b751ab39187e5749752a0ef033af09ff
Jaum Vitor Pires PC <joao.vitor.pires.060@gmail.com>
2020-06-04 02:49:55 -0300

    Revert "Poo - Banco"
    This reverts commit 6c7bd590a2834fb1116f492852039e4cc94f38a5.

--- Poo/aulas/banco_loiane/Conta.java
+++ Poo/aulas/banco_loiane/Conta.java
@@ -3,49 +3,32 @@ package banco_loiane;
 public class Conta {
     private int numero;
     private double saldo;
-    private StringBuilder registro = new StringBuilder();
     
     //Contrutor da classe: 
     public Conta(int numero) {
-    	//<> Cria uma conta com saldo zero
+    	//Cria uma conta com saldo zero
         this.numero = numero;
         this.saldo = 0;
     }
     
     public Conta(int numero, double saldo) {
-    	//<> Cria uma conta com valor saldo inicial
+    	//Cria uma conta com valor saldo inicial
         this.numero = numero;
         this.saldo = saldo; 
     }
     
-    public void buscarConta (int numConta) {
-    	//<> Busca a conta pelo numero dela
-    }
-    
-    public void extrato (double extrato, String locale) {
-    	//<> Armazena o extrato bancario da conta
-    	if (locale.equalsIgnoreCase("saque")) {
-    		this.registro.append("SACOU ----- R$" + extrato + "\n");
-    	}else if (locale.equalsIgnoreCase("deposito")) {
-    		this.registro.append("DEPOSITOU - R$" + extrato + "\n"); 
-    	}
-    }
-    
-    public String extrato () { //Retorna o extrato bancario da conta
-    	return String.format(this.registro.toString() + "Saldo atual: R$%s", this.saldo);
+    public void extrato () {
+    	//Imprime extrato bancario
     }
 
     public boolean depositaSaldo(double saldo) { 
-    	//<> Passando os registros, para colocar no extrato
     	setSaldo(getSaldo() + saldo); 
-    	extrato(saldo, "deposito"); 
     	return true; 
     }
     
     public boolean sacarSaldo(double saldo) {
     	if (this.saldo - saldo >= 0) {
     		this.saldo = getSaldo() - saldo;
-    		extrato(saldo, "saque"); //Passando os registros, para colocar no extrato
     		return true;
     	}
     	return false; 
--- Poo/aulas/banco_loiane/GerenciaConta.java
+++ Poo/aulas/banco_loiane/GerenciaConta.java
@@ -2,20 +2,9 @@ package banco_loiane;
 
 public class GerenciaConta {
 	private Conta[] vetor = new Conta[3];
-	private int cont=0;
 	
 	public void criarConta (int num) {
 		Conta x = new Conta (num);
-		this.vetor[cont] = x ;
-		this.cont++; 
-	}
-	
-	public Conta buscar (int numeroConta) {
-		for (int i=0; i<this.cont; i++) {
-			if (this.vetor[i].getNumero() == numeroConta) {
-				return vetor[i];
-			}
-		}
-		return null; 
+		vetor[0] = x ;
 	}
 }
