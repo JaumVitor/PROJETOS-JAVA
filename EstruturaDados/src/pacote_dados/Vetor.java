2478ff4d7b322907840b18f77d49a4b4bbcd1da8
Jaum Vitor Pires PC <joao.vitor.pires.060@gmail.com>
2020-06-04 02:59:26 -0300

    Ed/pacote_dados: Added aumentaCapacidade()

--- EstruturaDados/src/pacote_dados/Vetor.java
+++ EstruturaDados/src/pacote_dados/Vetor.java
@@ -6,27 +6,20 @@ public class Vetor {
 	protected int cont;  //variavel de controle do vetor
 	
 	//METODOS AUXILIARES
-	public void statusElementos() {
-		//Mostra todos os elementos do vetor, usando o toString: 
-		System.out.println("---------Imprimindo as palavras do vetor----------");
-		System.out.println(this.toString()); //Quando eu quero referenciar o toString da propria classe 
-		
-		/* Outro modelo de implementação (Imprimindo todos os elementos, pecorrendo o for): 
-		for (int i=0; i<cont; i++) {
-			System.out.println(this.vetor[i]);
+	private void aumentarCapacidade() {
+		if (this.cont == this.vetor.length) {
+			String vetorAux[] = new String[vetor.length * 2]; //Criando um vetor com o dobro do tamanho
+			for (int i=0; i<this.vetor.length; i++) {
+				vetorAux[i] = vetor[i];  
+			}
+			this.vetor = vetorAux; 
 		}
-		
-		  Segundo modelo de implementação: 
-		for (String osDados: vetor) {
-			if (osDados == null)  //Pecorrer todos os valores, caso não exista mais ele sai do loping
-				break; 
-			System.out.println();
-		}*/ 
 	}
-
+	
 	public boolean adicionar(String string) {
 		//adiciona elemento dentro do vetor
-		if (this.cont >= this.vetor.length) { return false; }
+		aumentarCapacidade(); 
+		if (this.cont >= this.vetor.length) { return false; } //Funcionaria caso o vetor podesse ficar cheio
 		
 		//Caso tenha espaço no vetor: 
 		vetor[cont] = string; 
@@ -36,6 +29,7 @@ public class Vetor {
 	
 	//Sobrecarregando metodo adicionar (Add na posicao pedida)
 	public boolean adicionar(String string, int pos) throws Exception{
+		aumentarCapacidade();
 		if (this.cont + 1 > vetor.length) {
 			throw new Exception ("Vetor esta cheio, e nao pode add elemento"); 
 		}else if (pos > this.cont || pos < 0 ) {
@@ -67,6 +61,8 @@ public class Vetor {
 		}
 		return -1; 
 	}
+	
+	//metodo que remove elementos do vetor
 	
 	//CONTRUTOR DA CLASSE VETOR
 	public Vetor (int tamanho) { //Recebendo o tamanho do vetor como parmetro
